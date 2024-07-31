07a2558b763c776c8bd6d6e20a60dcabf240e344
ninnccc <cytheriakii@gmail.com>
2024-08-01 00:18:37 +0800

    Delete count cat age directory

--- count cat age/cAge.js
+++
@@ -1,5 +0,0 @@
-function countMyCatAge(){
-  var catAge= document.getElementById("cat_age").value;
-  var catHumanAge = (catAge * 12) * 4 / 12;
-  document.getElementById("result").innerHTML = "貓咪" + catAge + "歲是人類的" + catHumanAge + "歲!";
-}
